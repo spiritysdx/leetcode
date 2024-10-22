a3f2d7edfad2465ec7eb9fca174278aa86df63ce
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-10-22 20:43:56 +0800

    Update 033.字母异位词分组.go

--- LCR/033.字母异位词分组.go
+++ LCR/033.字母异位词分组.go
@@ -32,32 +32,25 @@ func groupAnagrams(strs []string) [][]string {
 func groupAnagrams(strs []string) [][]string {
     // 创建一个映射，用于存储字符计数和对应的原始字符串数组
     mp := map[[26]int][]string{}
-
     // 遍历输入的字符串数组
     for _, str := range strs {
         // 创建一个长度为 26 的数组，用于计数每个字母出现的次数
         cnt := [26]int{}
-        
         // 遍历字符串中的每个字符
         for _, b := range str {
             // 计算字母的索引并增加对应计数
             cnt[b-'a']++
         }
-
         // 将原始字符串添加到对应字符计数的切片中
         mp[cnt] = append(mp[cnt], str)
     }
-
     // 创建一个切片用于存储结果，初始容量为映射的长度
     ans := make([][]string, 0, len(mp))
-
     // 遍历映射的值，将其添加到结果切片中
     for _, v := range mp {
         ans = append(ans, v)
     }
-
     // 返回结果切片
     return ans
 }
-
 **/
