e87c002c51c6bc9032ca67e8d2e624e84d5e72f5
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-10-24 19:54:30 +0800

    Create 交替打印数字和字母.go

---
+++ 面试题/交替打印数字和字母.go
@@ -0,0 +1,35 @@
+numbers, letters := make(chan bool), make(chan bool)
+wait := sync.WaitGroup{}
+go func(){
+  i := 1
+  for {
+    select {
+     case <- numbers:
+      fmt.Println(i)
+      i++
+      fmt.Println(i)
+      i++
+      letters <- true
+    } 
+  }
+}()
+wait.Add(1)
+go func(){
+  i := 'A'
+  for {
+    select {
+     case <- letters:
+      if z > 'Z' {
+        wait.Done()
+        return
+      }
+      fmt.Println(i)
+      i++
+      fmt.Println(i)
+      i++
+      numbers <- true
+    } 
+  }
+}(&wait)
+number<-true
+wait.Wait()
