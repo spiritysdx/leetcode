86ca812de67debe6f8485d2b8348b29413c72d80
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-11 20:31:46 +0800

    Update 088. 最小花费爬楼梯.go

--- LCR/088. 最小花费爬楼梯.go
+++ LCR/088. 最小花费爬楼梯.go
@@ -3,6 +3,9 @@ func minCostClimbingStairs(cost []int) int {
 	// 定义dp数组，dp[i]表示到达第i阶的最小花费
 	dp := make([]int, n+1)
 	// 动态规划计算
+	// 初始状态：
+	// 到达第 0 个阶梯不需要花费：dp[0] = 0；
+        // 到达第 1 个阶梯也不需要花费：dp[1] = 0。
 	for i := 2; i <= n; i++ {
 		dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2]) // 状态转移方程
 	}
