71945be44a0619d80f63b4ee9af85e3f2bd07ec2
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-14 14:50:30 +0800

    Update 150.层序遍历.go

--- LCR/150.层序遍历.go
+++ LCR/150.层序遍历.go
@@ -7,34 +7,61 @@
  * }
  */
 
-func decorateRecord(root *TreeNode) (ans [][]int) {
-    // 如果根节点为空，直接返回空结果
+// func decorateRecord(root *TreeNode) (ans [][]int) {
+//     // 如果根节点为空，直接返回空结果
+//     if root == nil {
+//         return
+//     }
+//     // 初始化当前层的节点列表，初始值为根节点
+//     cur := []*TreeNode{root}
+//     // 开始层序遍历
+//     for len(cur) > 0 {
+//         nxt := []*TreeNode{} // 下一层的节点列表
+//         vals := make([]int, len(cur)) // 预分配空间，用于存储当前层节点的值
+//         // 遍历当前层的所有节点
+//         for i, node := range cur {
+//             vals[i] = node.Val // 记录当前节点的值
+//             // 如果左子节点存在，加入下一层节点列表
+//             if node.Left != nil {
+//                 nxt = append(nxt, node.Left)
+//             }
+//             // 如果右子节点存在，加入下一层节点列表
+//             if node.Right != nil {
+//                 nxt = append(nxt, node.Right)
+//             }
+//         }
+//         // 更新当前层为下一层
+//         cur = nxt
+//         // 将当前层的值加入结果集
+//         ans = append(ans, vals)
+//     }
+//     // 返回结果
+//     return
+// }
+
+
+func decorateRecord(root *TreeNode) [][]int {
+    res := [][]int{}
     if root == nil {
-        return
+        return res
     }
-    // 初始化当前层的节点列表，初始值为根节点
     cur := []*TreeNode{root}
-    // 开始层序遍历
     for len(cur) > 0 {
-        nxt := []*TreeNode{} // 下一层的节点列表
-        vals := make([]int, len(cur)) // 预分配空间，用于存储当前层节点的值
-        // 遍历当前层的所有节点
-        for i, node := range cur {
-            vals[i] = node.Val // 记录当前节点的值
-            // 如果左子节点存在，加入下一层节点列表
-            if node.Left != nil {
-                nxt = append(nxt, node.Left)
+        nxt := []*TreeNode{}
+        vals := []int{}
+        // 遍历当前层所有节点
+        for _, v := range cur {
+            vals = append(vals, v.Val)
+            if v.Left != nil {
+                nxt = append(nxt, v.Left)
             }
-            // 如果右子节点存在，加入下一层节点列表
-            if node.Right != nil {
-                nxt = append(nxt, node.Right)
+            if v.Right != nil {
+                nxt = append(nxt, v.Right)
             }
         }
-        // 更新当前层为下一层
+        // 到下一层之前的操作
         cur = nxt
-        // 将当前层的值加入结果集
-        ans = append(ans, vals)
+        res = append(res, vals)
     }
-    // 返回结果
-    return
+    return res
 }
