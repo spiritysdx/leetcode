8c2979edee94a875d75336694990f46d8a2766ac
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-15 15:48:23 +0800

    Create 2300. 咒语和药水的成功对数.go

---
+++ 二分法/2300. 咒语和药水的成功对数.go
@@ -0,0 +1,30 @@
+func successfulPairs(spells []int, potions []int, success int64) []int {
+    sizeA, sizeB := len(spells), len(potions)
+    res := []int{}
+    if sizeA == 0 || sizeB == 0 {
+        return res
+    }
+    slices.Sort(potions)
+    for _, k := range spells {
+        res = append(res, calculate(potions, int64(k), success))
+    }
+    return res
+}
+
+func calculate(temp []int, k int64, target int64) int {
+    left := 0
+    right := len(temp)
+    for left < right {
+        middle := left + (right-left)/2
+        if k*int64(temp[middle]) < target {
+            left = middle+1
+        } else {
+            right = middle
+        }
+    }
+    start := left // 从这开始后面的都可以
+    if start == len(temp) {
+        return 0
+    }
+    return len(temp) - start
+}
