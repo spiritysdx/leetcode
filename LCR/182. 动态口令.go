5536a7a9f6e9b564e5f4e1f6d74684f3643bb7fa
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-15 17:00:13 +0800

    Update 182. 动态口令.go

--- LCR/182. 动态口令.go
+++ LCR/182. 动态口令.go
@@ -12,3 +12,20 @@ func dynamicPassword(password string, target int) string {
     res2 = append(res2, res1...)
     return string(res2)
 }
+
+// func dynamicPassword(s string, n int) string {
+//     length := len(s)
+//     b := make([]byte, length) // 辅助数组
+//     idx := 0
+//     // 写入尾部字符串
+//     for i := n; i < length; i++ {
+//         b[idx] = s[i]
+//         idx++
+//     }
+//     // 写入头部字符串
+//     for i := 0; i < n; i++ {
+//         b[idx] = s[i]
+//         idx++
+//     }
+//     return string(b)
+// }
