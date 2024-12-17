06bdf3260a946785540a754f2000c4ee4ae5c7f1
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-17 11:06:41 +0800

    Create 235. 二叉搜索树的最近公共祖先.go

---
+++ 最近公共祖先/235. 二叉搜索树的最近公共祖先.go
@@ -0,0 +1,30 @@
+// https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-tree/solutions/2023872/fen-lei-tao-lun-luan-ru-ma-yi-ge-shi-pin-2r95/
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+    if root == nil {
+        return nil
+    }
+    if root == q || root == p {
+        return root
+    }
+    left := lowestCommonAncestor(root.Left, p, q)
+    right := lowestCommonAncestor(root.Right, p, q)
+    if left != nil && right != nil {
+        return root
+    }
+    if left != nil && right == nil {
+        return left
+    }
+    if left == nil && right != nil {
+        return right
+    }
+    return nil
+}
