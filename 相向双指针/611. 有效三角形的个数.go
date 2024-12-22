7b04e66defa1bedb530b4c01c3f6ff8129dcbd0c
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-22 15:15:49 +0800

    Update 611. 有效三角形的个数.go

--- 相向双指针/611. 有效三角形的个数.go
+++ 相向双指针/611. 有效三角形的个数.go
@@ -19,3 +19,35 @@ func triangleNumber(nums []int) int {
     }
     return res
 }
+
+
+// func triangleNumber(nums []int) (ans int) {
+//     slices.Sort(nums)
+//     for k := 2; k < len(nums); k++ {
+//         c := nums[k]
+//         i := 0     // a=nums[i]
+//         j := k - 1 // b=nums[j]
+//         for i < j {
+//             if nums[i]+nums[j] > c {
+//                 // 由于 nums 已经从小到大排序
+//                 // nums[i]+nums[j] > c 同时意味着：
+//                 // nums[i+1]+nums[j] > c
+//                 // nums[i+2]+nums[j] > c
+//                 // ...
+//                 // nums[j-1]+nums[j] > c
+//                 // 从 i 到 j-1 一共 j-i 个
+//                 ans += j - i
+//                 j--
+//             } else {
+//                 // 由于 nums 已经从小到大排序
+//                 // nums[i]+nums[j] <= c 同时意味着
+//                 // nums[i]+nums[j-1] <= c
+//                 // ...
+//                 // nums[i]+nums[i+1] <= c
+//                 // 所以在后续的内层循环中，nums[i] 不可能作为三角形的边长，没有用了
+//                 i++
+//             }
+//         }
+//     }
+//     return ans
+// }
