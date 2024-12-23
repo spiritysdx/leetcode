33d6a1b59fa9be049dae24aa71651d8ac1607190
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-23 17:26:12 +0800

    Create 2962. 统计最大元素出现至少 K 次的子数组.go

---
+++ 同向双指针-滑动窗口/2962. 统计最大元素出现至少 K 次的子数组.go
@@ -0,0 +1,38 @@
+// https://leetcode.cn/problems/count-subarrays-where-max-element-appears-at-least-k-times/
+func countSubarrays(nums []int, k int) int64 {
+    if len(nums) == 0 || k <= 0 {
+        return 0
+    }
+    // 找到数组中的最大值
+    maxNum := 0
+    for _, v := range nums {
+        maxNum = max(maxNum, v)
+    }
+    // 滑动窗口计数
+    ans := 0
+    start := 0
+    temp := 0
+    size := len(nums)
+    for end, x := range nums {
+        if x == maxNum {
+            temp += 1
+        }
+        for temp >= k {
+            // 累加以 start 开头、以 end 结尾的子数组数量
+            ans += size - end
+            if nums[start] == maxNum {
+                temp -= 1
+            }
+            start += 1
+        }
+    }
+    return int64(ans)
+}
+
+// 比较两个整数的最大值
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
