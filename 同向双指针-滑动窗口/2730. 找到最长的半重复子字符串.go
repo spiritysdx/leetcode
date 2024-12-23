7765a21f4557fddcd28ae8e0a08db8da9e2e7152
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-23 14:05:57 +0800

    Update 2730. 找到最长的半重复子字符串.go

--- 同向双指针-滑动窗口/2730. 找到最长的半重复子字符串.go
+++ 同向双指针-滑动窗口/2730. 找到最长的半重复子字符串.go
@@ -28,3 +28,24 @@ func max(a, b int) int {
     }
     return b
 }
+
+// func longestSemiRepetitiveSubstring(s string) int {
+// 	ans, left, same := 1, 0, 0
+// 	for right := 1; right < len(s); right++ {
+// 		if s[right] == s[right-1] {
+// 			same++
+// 		}
+//         if same > 1 { // same == 2
+//             left++
+//             for s[left] != s[left-1] {
+//                 left++
+//             }
+//             same = 1
+//         }
+// 		ans = max(ans, right-left+1)
+// 	}
+// 	return ans
+// }
+// 移动右指针 right，并统计相邻相同的情况出现了多少次，记作 same。
+// 如果 same>1，则不断移动左指针 left 直到 s[left]=s[left−1]，此时将一对相同的字符移到窗口之外。然后将 same 置为 1。
+// 然后统计子串长度 right−left+1 的最大值。
