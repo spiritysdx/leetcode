cc212543bf9281e2ce66af1363b1810bbf3ff2e9
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-24 16:50:56 +0800

    Update 162. 寻找峰值.go

--- 二分查找/162. 寻找峰值.go
+++ 二分查找/162. 寻找峰值.go
@@ -24,3 +24,15 @@ func findPeakElement(nums []int) int {
 }
 
 // 二分法
+func findPeakElement(nums []int) int {
+    left, right := -1, len(nums)-1 // 初始化左右边界为开区间 (-1, n-1)
+    for left+1 < right { // 当开区间不为空时继续循环
+        mid := left + (right-left)/2 // 计算中间位置，防止溢出
+        if nums[mid] > nums[mid+1] { // 如果当前元素大于右侧元素
+            right = mid // 峰值可能在左侧或就是当前元素，更新右边界
+        } else { // 如果当前元素小于或等于右侧元素
+            left = mid // 峰值在右侧，更新左边界
+        }
+    }
+    return right // 返回右边界，即峰值元素的索引
+}
