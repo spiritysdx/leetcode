6d456033068ee714c5cceb676a5caff5138bb5ec
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-26 15:12:18 +0800

    Create 237. 删除链表中的节点.go

---
+++ 前后指针-链表删除去重/237. 删除链表中的节点.go
@@ -0,0 +1,12 @@
+// https://leetcode.cn/problems/delete-node-in-a-linked-list/description/
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteNode(node *ListNode) {
+    node.Val = node.Next.Val
+    node.Next = node.Next.Next
+}
