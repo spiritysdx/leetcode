623c9b61d8d54dc3fe127c3add2b6b1516fab944
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-26 16:14:18 +0800

    Create 203. 移除链表元素.go

---
+++ 前后指针-链表删除去重/203. 移除链表元素.go
@@ -0,0 +1,20 @@
+// https://leetcode.cn/problems/remove-linked-list-elements/
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeElements(head *ListNode, val int) *ListNode {
+    dummy := &ListNode{Next: head}
+    cur := dummy
+    for cur != nil && cur.Next != nil {
+        if cur.Next.Val == val {
+            cur.Next = cur.Next.Next
+        } else {
+            cur = cur.Next
+        }
+    }
+    return dummy.Next
+}
