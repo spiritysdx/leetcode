800877726105d5cc05f95919df1b4c83f9235f6c
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-26 19:26:17 +0800

    Create 112. 路径总和.go

---
+++ 二叉树-递归数学归纳法栈/112. 路径总和.go
@@ -0,0 +1,31 @@
+// https://leetcode.cn/problems/path-sum/description/
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func hasPathSum(root *TreeNode, targetSum int) bool {
+    // 自顶向下
+    var ans bool
+    var dfs func(*TreeNode, int)
+    dfs = func(node *TreeNode, cal int) {
+        if node == nil {
+            return
+        }
+        cal += node.Val
+        if node.Left != nil {
+            dfs(node.Left, cal)
+        }
+        if node.Right != nil {
+            dfs(node.Right, cal)
+        }
+        if node.Left == nil && node.Right == nil && cal == targetSum {
+            ans = true
+        }
+    }
+    dfs(root, 0)
+    return ans
+}
