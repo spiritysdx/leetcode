2f797c0f4f255349ee58cafa2f9c4906369ae5c8
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-26 18:33:56 +0800

    Update 104. 二叉树的最大深度.go

--- 二叉树-递归数学归纳法栈/104. 二叉树的最大深度.go
+++ 二叉树-递归数学归纳法栈/104. 二叉树的最大深度.go
@@ -1,1 +1,24 @@
 // https://leetcode.cn/problems/maximum-depth-of-binary-tree/description/
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+    if root == nil { // 边界条件
+        return 0
+    }
+    L := maxDepth(root.Left) // 递
+    R := maxDepth(root.Right)
+    return max(L, R)+1 // 归
+}
+
+func max(a,b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
