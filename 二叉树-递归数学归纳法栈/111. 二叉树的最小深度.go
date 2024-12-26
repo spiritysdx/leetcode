3922978fa817dfb05cb933f9057f81a2109589a1
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-26 19:06:57 +0800

    Update 111. 二叉树的最小深度.go

--- 二叉树-递归数学归纳法栈/111. 二叉树的最小深度.go
+++ 二叉树-递归数学归纳法栈/111. 二叉树的最小深度.go
@@ -41,3 +41,16 @@ func min(a,b int) int {
     }
     return a
 }
+
+// func minDepth(root *TreeNode) int {
+//     if root == nil {
+//         return 0
+//     }
+//     if root.Right == nil {
+//         return minDepth(root.Left) + 1
+//     }
+//     if root.Left == nil {
+//         return minDepth(root.Right) + 1
+//     }
+//     return min(minDepth(root.Left), minDepth(root.Right)) + 1
+// }
