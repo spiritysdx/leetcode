a3dcbc296aa13ba709e3f8363d19459bad97de7b
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-27 10:12:48 +0800

    Create 129. 求根节点到叶节点数字之和.go

---
+++ 二叉树-递归数学归纳法栈/129. 求根节点到叶节点数字之和.go
@@ -0,0 +1,31 @@
+// https://leetcode.cn/problems/sum-root-to-leaf-numbers/description/
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sumNumbers(root *TreeNode) int {
+    ans := 0
+    // 深度优先搜索函数
+    var dfs func(*TreeNode, int)
+    dfs = func(node *TreeNode, currentSum int) {
+        if node == nil {
+            return
+        }
+        // 更新当前路径的数字
+        currentSum = currentSum*10 + node.Val
+        // 如果到达叶子节点，将当前路径数字加入总和
+        if node.Left == nil && node.Right == nil {
+            ans += currentSum
+            return
+        }
+        // 递归访问左子树和右子树
+        dfs(node.Left, currentSum)
+        dfs(node.Right, currentSum)
+    }
+    dfs(root, 0)
+    return ans
+}
