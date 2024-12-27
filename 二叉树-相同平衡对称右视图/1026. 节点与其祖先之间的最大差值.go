1b780d46585dd07e4a86978dccb50bf7cff4327e
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-27 19:35:49 +0800

    Update 1026. 节点与其祖先之间的最大差值.go

--- 二叉树-相同平衡对称右视图/1026. 节点与其祖先之间的最大差值.go
+++ 二叉树-相同平衡对称右视图/1026. 节点与其祖先之间的最大差值.go
@@ -38,3 +38,36 @@ func abs(a int) int {
     }
     return a
 }
+
+// 优化方法：在递归过程中只传递当前路径中的最大值和最小值，而不是整个路径的值数组。这样可以显著降低空间复杂度和运行时间。
+// func maxAncestorDiff(root *TreeNode) int {
+//     if root == nil {
+//         return 0
+//     }
+//     var dfs func(node *TreeNode, minVal, maxVal int) int
+//     dfs = func(node *TreeNode, minVal, maxVal int) int {
+//         if node == nil {
+//             return maxVal - minVal
+//         }
+//         // 更新当前路径的最小值和最大值
+//         minVal = min(minVal, node.Val)
+//         maxVal = max(maxVal, node.Val)
+//         // 递归计算左右子树的结果
+//         left := dfs(node.Left, minVal, maxVal)
+//         right := dfs(node.Right, minVal, maxVal)
+//         return max(left, right)
+//     }
+//     return dfs(root, root.Val, root.Val)
+// }
+// func min(a, b int) int {
+//     if a < b {
+//         return a
+//     }
+//     return b
+// }
+// func max(a, b int) int {
+//     if a > b {
+//         return a
+//     }
+//     return b
+// }
