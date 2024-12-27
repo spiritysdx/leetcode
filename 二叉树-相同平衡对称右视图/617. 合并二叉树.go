3f547e759fd44ae3ef88c16ba385601f67b77837
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-27 18:50:14 +0800

    Update 617. 合并二叉树.go

--- 二叉树-相同平衡对称右视图/617. 合并二叉树.go
+++ 二叉树-相同平衡对称右视图/617. 合并二叉树.go
@@ -23,3 +23,20 @@ func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
     ans.Val = root1.Val + root2.Val
     return ans
 }
+
+// func mergeTrees(root1, root2 *TreeNode) *TreeNode {
+//     if root1 == nil {
+//         return root2
+//     }
+//     if root2 == nil {
+//         return root1
+//     }
+//     return &TreeNode{root1.Val + root2.Val,
+//         mergeTrees(root1.Left, root2.Left),   // 合并左子树
+//         mergeTrees(root1.Right, root2.Right)} // 合并右子树
+// }
+/*
+如果 root1​ 是空的，无需合并，直接返回 root2​。
+如果 root2​ 是空的，无需合并，直接返回 root1​。
+如果都不为空，那么将这两个节点的值相加，作为合并后节点的新值。然后递归合并 root1​.left 与 root2​.left，得到合并后的左子树；递归合并 root1​.right 与 root2​.right，得到合并后的右子树。最后返回合并后的节点。
+*/
