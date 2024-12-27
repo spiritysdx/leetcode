92a6c429b0676af0499af783ecb00d5d4c637935
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-27 19:50:06 +0800

    Create 1372. 二叉树中的最长交错路径.go

---
+++ 二叉树-相同平衡对称右视图/1372. 二叉树中的最长交错路径.go
@@ -0,0 +1,35 @@
+// https://leetcode.cn/problems/longest-zigzag-path-in-a-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func longestZigZag(root *TreeNode) int {
+    var maxLen int
+    var dfs func(node *TreeNode, isLeft bool, depth int)
+    dfs = func(node *TreeNode, isLeft bool, depth int) {
+        if node == nil {
+            return
+        }
+        // 更新最大长度
+        if depth > maxLen {
+            maxLen = depth
+        }
+        if isLeft {
+            // 当前方向为左，下一步走右
+            dfs(node.Left, false, depth+1) // 继续左子树，重置深度
+            dfs(node.Right, true, 1)      // 切换方向，从右子树开始
+        } else {
+            // 当前方向为右，下一步走左
+            dfs(node.Right, true, depth+1) // 继续右子树，重置深度
+            dfs(node.Left, false, 1)      // 切换方向，从左子树开始
+        }
+    }
+    // 从根节点的两种方向开始搜索
+    dfs(root.Left, false, 1)
+    dfs(root.Right, true, 1)
+    return maxLen
+}
