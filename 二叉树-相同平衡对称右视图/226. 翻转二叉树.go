190ebf3239c8196d37ad5aed1648c4aaae040016
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-27 18:33:03 +0800

    Create 226. 翻转二叉树.go

---
+++ 二叉树-相同平衡对称右视图/226. 翻转二叉树.go
@@ -0,0 +1,22 @@
+// https://leetcode.cn/problems/invert-binary-tree/description/
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+    var dfs func(*TreeNode) 
+    dfs = func(node *TreeNode) {
+        if node == nil {
+            return
+        }
+        node.Left, node.Right = node.Right, node.Left
+        dfs(node.Left)
+        dfs(node.Right)
+    }
+    dfs(root)
+    return root
+}
