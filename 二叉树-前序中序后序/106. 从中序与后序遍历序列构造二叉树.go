dcfc2f8979ef3c5f9a527ae29f47e88d61d28f33
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-29 12:52:01 +0800

    Create 106. 从中序与后序遍历序列构造二叉树.go

---
+++ 二叉树-前序中序后序/106. 从中序与后序遍历序列构造二叉树.go
@@ -0,0 +1,20 @@
+// https://leetcode.cn/problems/construct-binary-tree-from-inorder-and-postorder-traversal/description/
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func buildTree(inorder []int, postorder []int) *TreeNode {
+    size := len(postorder)
+    if size == 0 {
+        return nil
+    }
+    index := slices.Index(inorder, postorder[size-1]) // 左子树大小
+    node := &TreeNode{Val: postorder[size-1]}
+    node.Left = buildTree(inorder[:index], postorder[:index])
+	node.Right = buildTree(inorder[index+1:], postorder[index:len(postorder)-1])
+    return node
+}
