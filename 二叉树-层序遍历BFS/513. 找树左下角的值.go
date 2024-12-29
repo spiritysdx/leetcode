0606c0b7e6e0a94f4b12a2da64f054bb410a9619
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-29 19:39:44 +0800

    Update 513. 找树左下角的值.go

--- 二叉树-层序遍历BFS/513. 找树左下角的值.go
+++ 二叉树-层序遍历BFS/513. 找树左下角的值.go
@@ -25,3 +25,18 @@ func findBottomLeftValue(root *TreeNode) int {
     }
     return ans
 }
+
+func findBottomLeftValue(root *TreeNode) int {
+    node := root
+    q := []*TreeNode{root}
+    for len(q) > 0 {
+        node, q = q[0], q[1:]
+        if node.Right != nil {
+            q = append(q, node.Right)
+        }
+        if node.Left != nil {
+            q = append(q, node.Left)
+        }
+    }
+    return node.Val
+}
