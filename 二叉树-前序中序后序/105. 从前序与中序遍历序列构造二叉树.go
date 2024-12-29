4d25a768751cc5a3bccc4c343a381c5b8155e066
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-12-29 11:33:23 +0800

    Update 105. 从前序与中序遍历序列构造二叉树.go

--- 二叉树-前序中序后序/105. 从前序与中序遍历序列构造二叉树.go
+++ 二叉树-前序中序后序/105. 从前序与中序遍历序列构造二叉树.go
@@ -1,11 +1,11 @@
 // https://leetcode.cn/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
 func buildTree(preorder, inorder []int) *TreeNode {
-    n := len(preorder)
-    if n == 0 { // 空节点
+    n := len(preorder) // 获取前序遍历数组的长度
+    if n == 0 { // 如果长度为 0，说明是空节点，返回 nil
         return nil
     }
-    leftSize := slices.Index(inorder, preorder[0]) // 左子树的大小
-    left := buildTree(preorder[1:1+leftSize], inorder[:leftSize])
-    right := buildTree(preorder[1+leftSize:], inorder[1+leftSize:])
-    return &TreeNode{preorder[0], left, right}
+    leftSize := slices.Index(inorder, preorder[0]) // 找到根节点在中序遍历中的索引，计算左子树的大小
+    left := buildTree(preorder[1:1+leftSize], inorder[:leftSize]) // 递归构建左子树
+    right := buildTree(preorder[1+leftSize:], inorder[1+leftSize:]) // 递归构建右子树
+    return &TreeNode{preorder[0], left, right} // 构造当前节点并返回
 }
