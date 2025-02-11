115a663b2045d46604a8f042da78f4c8e3226443
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2025-02-11 09:18:34 +0800

    Update 交替打印数字和字母.go

--- 面试题/交替打印数字和字母.go
+++ 面试题/交替打印数字和字母.go
@@ -1,35 +1,44 @@
-numbers, letters := make(chan bool), make(chan bool)
-wait := sync.WaitGroup{}
-go func(){
-  i := 1
-  for {
-    select {
-     case <- numbers:
-      fmt.Println(i)
-      i++
-      fmt.Println(i)
-      i++
-      letters <- true
-    } 
-  }
-}()
-wait.Add(1)
-go func(wait *sync.WaitGroup){
-  i := 'A'
-  for {
-    select {
-     case <- letters:
-      if i > 'Z' {
-        wait.Done()
-        return
-      }
-      fmt.Println(i)
-      i++
-      fmt.Println(i)
-      i++
-      numbers <- true
-    } 
-  }
-}(&wait)
-number<-true
-wait.Wait()
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	numbers, letters := make(chan bool), make(chan bool)
+	wait := sync.WaitGroup{}
+	go func() {
+		i := 1
+		for {
+			select {
+			case <-numbers:
+				fmt.Println(i)
+				i++
+				fmt.Println(i)
+				i++
+				letters <- true
+			}
+		}
+	}()
+	wait.Add(1)
+	go func(wait *sync.WaitGroup) {
+		i := 'A'
+		for {
+			select {
+			case <-letters:
+				if i > 'Z' {
+					wait.Done()
+					return
+				}
+				fmt.Println(i)
+				i++
+				fmt.Println(i)
+				i++
+				numbers <- true
+			}
+		}
+	}(&wait)
+	numbers <- true
+	wait.Wait()
+}
