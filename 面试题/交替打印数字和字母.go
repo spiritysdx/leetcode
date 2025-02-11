74df7f7681a9192cc027feadd64eec6a9921a9b4
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2025-02-11 09:20:11 +0800

    Update 交替打印数字和字母.go

--- 面试题/交替打印数字和字母.go
+++ 面试题/交替打印数字和字母.go
@@ -1,44 +1,46 @@
 package main
 
 import (
-	"fmt"
-	"sync"
+    "fmt"
+    "sync"
 )
 
 func main() {
-	numbers, letters := make(chan bool), make(chan bool)
-	wait := sync.WaitGroup{}
-	go func() {
-		i := 1
-		for {
-			select {
-			case <-numbers:
-				fmt.Println(i)
-				i++
-				fmt.Println(i)
-				i++
-				letters <- true
-			}
-		}
-	}()
-	wait.Add(1)
-	go func(wait *sync.WaitGroup) {
-		i := 'A'
-		for {
-			select {
-			case <-letters:
-				if i > 'Z' {
-					wait.Done()
-					return
-				}
-				fmt.Println(i)
-				i++
-				fmt.Println(i)
-				i++
-				numbers <- true
-			}
-		}
-	}(&wait)
-	numbers <- true
-	wait.Wait()
+    numbers, letters := make(chan bool), make(chan bool)
+    wait := sync.WaitGroup{}
+    
+    // 打印数字的 goroutine
+    go func() {
+        i := 1
+        for {
+            select {
+            case <-numbers:
+                fmt.Println(i)
+                i++
+                letters <- true
+            }
+        }
+    }()
+    
+    // 打印字母的 goroutine
+    wait.Add(1)
+    go func(wait *sync.WaitGroup) {
+        i := 'A'
+        for {
+            select {
+            case <-letters:
+                if i > 'Z' {
+                    wait.Done()
+                    return
+                }
+                fmt.Println(string(i))
+                i++
+                numbers <- true
+            }
+        }
+    }(&wait)
+    
+    // 启动第一个数字的打印
+    numbers <- true
+    wait.Wait()
 }
