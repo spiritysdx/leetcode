706ad47b2379ef0b836ba9a546142f4f9073582d
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2025-02-12 10:27:43 +0800

    Update 一个timer统一取消协程.go

--- 面试题/一个timer统一取消协程.go
+++ 面试题/一个timer统一取消协程.go
@@ -25,19 +25,16 @@ func main() {
 	var wg sync.WaitGroup
 	ctx, cancel := context.WithCancel(context.Background()) // 统一的 ctx 控制所有协程
 	defer cancel()
-
 	// 外部使用 Timer 触发取消
 	timer := time.AfterFunc(5*time.Second, func() {
 		fmt.Println("Timeout reached! Stopping all workers...")
 		cancel() // 触发全局取消
 	})
-
 	// 启动多个 worker
 	for i := 1; i <= 3; i++ {
 		wg.Add(1)
 		go worker(ctx, i, &wg)
 	}
-
 	wg.Wait() // 等待所有 worker 退出
 	timer.Stop() // 确保 timer 释放
 	fmt.Println("All workers stopped, main function exits.")
